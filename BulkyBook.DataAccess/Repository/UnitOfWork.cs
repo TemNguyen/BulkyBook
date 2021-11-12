00269d0ce2fcf210fff393b79c35f19a7b627c06
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2021-11-12 22:56:38 +0700

    session 10.5

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -19,6 +19,7 @@ namespace BulkyBook.DataAccess.Repository
             SP_Call = new SP_Call(_db);
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
@@ -26,6 +27,7 @@ namespace BulkyBook.DataAccess.Repository
 
         public ICoverTypeRepository CoverType { get; private set; }
         public IProductRepository Product { get; private set; }
+        public ICompanyRepository Company { get; private set; }
 
         public void Dispose()
         {
